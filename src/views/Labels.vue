435687b4967d4ef4d8722bb635aa343dfa1169b4
monaaaaaa2544 <1227653652@qq.com>
2022-02-21 14:17:47 +0800

    '重复三次布局'

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-        lab
+    <div class="nav-wrapper">
+        <div class="content">
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+            <p>labels</p>
+
+        </div>
         <Nav></Nav>
     </div>
 </template>
@@ -11,4 +29,15 @@
 
 <style lang="scss" scoped>
 
+.nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.content{
+    overflow: auto;
+    flex-grow: 1;
+}
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,38 @@
 <template>
-    <div>
-        sta
+    <div class="nav-wrapper">
+        <div class="content">
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+            <p>statistics</p>
+
+        </div>
         <Nav></Nav>
     </div>
 </template>
@@ -10,4 +42,15 @@
 
 <style lang="scss" scoped>
 
+.nav-wrapper{
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+}
+
+.content{
+    overflow: auto;
+    flex-grow: 1;
+}
 </style>
